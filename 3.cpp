e2abe226a8f29781403444eab89af3d04e62ae6c
DELL <b710zbw@qq.com>
2022-09-21 15:32:42 +0800

    Google Format

--- 3.cpp
+++ 3.cpp
@@ -8,12 +8,11 @@ void mySwap(Rank &a, Rank &b) {
   b = temp;
 }
 
-void myReverse(Rank* A, Rank size) {
+void myReverse(Rank *A, Rank size) {
   Rank low = 0, high = size;
 
   while (low < high)
     mySwap(A[low++], A[--high]);
-  
 }
 
 int main() {
@@ -24,14 +23,14 @@ int main() {
   for (size_t i = 0; i < n; ++i) {
     cin >> A[i];
   }
-  
+
   myReverse(A, n);
   myReverse(A, k % n);
   myReverse(A + k % n, n - k % n);
 
   for (Rank i = 0; i < n; ++i) {
-    (i == n-1) ? (cout << A[i]) : (cout << A[i] << " ");
+    (i == n - 1) ? (cout << A[i]) : (cout << A[i] << " ");
   }
-  
+
   return 0;
 }
--- C3. 列表/listnode.hpp
+++ C3. 列表/listnode.hpp
@@ -4,44 +4,46 @@
 using Rank = int; //秩
 
 template <typename T> class ListNode;
-template <typename T> using ListNodePosi = ListNode<T>*; //列表节点位置
+template <typename T>
+using ListNodePosi = ListNode<T> *; //列表节点位置
 
-template <typename T> 
+template <typename T>
 class ListNode { //列表节点模板类
  private:
-  T data; //数值
+  T data;              //数值
   ListNodePosi<T> pre; //前驱节点
   ListNodePosi<T> suc; //后继节点
 
  public:
-//构造函数
-  ListNode {}; //默认构造函数，针对头节点header和尾节点trailer构造
-  ListNode (T e, ListNodePosi<T> p = NULL, ListNodePosi<T> s = NULL)
-        : data(e), pre(p), suc(s) {}; //默认构造器
-  
-//操作接口
-  ListNodePosi<T> pred() { return pre; } //获得前驱节点位置
-  ListNodePosi<T> succ() { return suc; }
-  T data() { return data; } //获得当前节点索寸数据对象
+   //构造函数
+   ListNode{}; //默认构造函数，针对头节点header和尾节点trailer构造
+   ListNode(T e, ListNodePosi<T> p = NULL, ListNodePosi<T> s = NULL)
+       : data(e), pre(p), suc(s){}; //默认构造器
 
-  ListNodePosi<T> insertAsPred(T const& e); //插入前驱节点，返回其位置
-  ListNodePosi<T> insertAsSucc(T const& e); //插入后继节点，返回其位置
+   //操作接口
+   ListNodePosi<T> pred() { return pre; } //获得前驱节点位置
+   ListNodePosi<T> succ() { return suc; } //获得后继节点位置
+   T data() { return data; }              //获得当前节点所存数据对象
+
+   ListNodePosi<T> insertAsPred(T const &e); //插入前驱节点，返回其位置
+   ListNodePosi<T> insertAsSucc(T const &e); //插入后继节点，返回其位置
 };
 
-template <typename T> 
-ListNodePosi<T> ListNode<T>::insertAsPred(T const& e) {
-  ListNodePosi<T> temp = new ListNode<T> (e, pre, this); //开辟堆空间，设置新节点
-  pre->suc = temp; 
+//实现部分
+template <typename T>
+ListNodePosi<T> ListNode<T>::insertAsPred(T const &e) {
+  ListNodePosi<T> temp = new ListNode<T>(e, pre, this); //开辟堆空间，设置新节点
+  pre->suc = temp;
   pre = temp; //修正链接
 
   return temp;
 }
 
 template <typename T>
-ListNodePosi<T> ListNode<T>::insertAsSucc(T const& e) {
-  ListNodePosi<T> temp = new ListNode<T> (e, this, suc);
+ListNodePosi<T> ListNode<T>::insertAsSucc(T const &e) {
+  ListNodePosi<T> temp = new ListNode<T>(e, this, suc);
   suc->pre = temp;
-  suc = temp; //this->suc = temp;
+  suc = temp; // this->suc = temp;
 
   return temp;
 }
